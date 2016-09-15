fae0fd3a63392d34be256e563bc629e67c258888
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-09-15 14:15:37 -0500

    cleaned up code and added comments

--- app.js
+++ app.js
@@ -1,26 +1,40 @@
-function truncateString(str, num) {
-  //console.log(str.length);
-  //console.log(num);
-  if (str.length > num) {
+/*******************************************************************************
+*                            Truncate a String                                 *
+*  Truncate a string (first argument) if it is longer than the given maximum   *
+*  string length (second argument). Return the truncated string with a         *
+*  ... ending.                                                                 *
+*******************************************************************************/
+function truncateString(str, num)
+{
+  // Test first and second argument
+  if (str.length > num && num > 3)
+  {
+    // Substring starting at character 0 and ending at num -3
+    strHold = str.substring(0,num-3);
 
-    strHold = str.substring(0,Math.min(num, Math.abs(num-3)));
-    //console.log(strHold + "...");
     return strHold + "...";
   }
-  else{
+  else if (str.length > num && num <= 3)
+  {
+    // Substring starting at character 0 and ending at num
+    strHold = str.substring(0,num);
+
+    return strHold + "...";
+  }
+  else
+  {
     return str;
   }
-
-
 }
 
+// Input Data
 console.log(truncateString("A-tisket a-tasket A green and yellow basket", 11));
 console.log(truncateString("Peter Piper picked a peck of pickled peppers", 14));
 console.log(truncateString("A-", 1));
 console.log(truncateString("Absolutely Longer", 2));
 
-console.log("Absolutely Longer".substring(-1));
-console.log("Absolutely Longer".slice(-1));
-
-//console.log("A-tisket a-tasket".length);
-//console.log("A-tisket a-tasket".slice(3));
+//Output Data
+//A-tisket...
+//Peter Piper...
+//A...
+//Ab...
